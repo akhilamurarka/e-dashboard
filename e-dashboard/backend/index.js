1deb6955d100f86465b4e463f3dcd914d1eb7e57
akhil <akhilamurarka@gmail.com>
2025-06-05 15:29:07 +0530

    testing mongodb

--- e-dashboard/backend/index.js
+++ e-dashboard/backend/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
-
+require("./db/config");
+const User = require("./db/User");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("app is working");
+app.use(express.json());
+
+app.post("/register", async (req, resp) => {
+  const user = new User(req.body);
+  const result = await user.save();
+  resp.send(result);
 });
 
 app.listen(5000);
