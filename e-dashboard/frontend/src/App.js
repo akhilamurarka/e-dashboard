69d965722ea5c694be951f79ca442ba177637309
akhil <akhilamurarka@gmail.com>
2025-06-07 23:30:18 +0530

    completed signup flow

--- e-dashboard/frontend/src/App.js
+++ e-dashboard/frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignUp";
+import PrivateComponent from "./components/PrivateComponent";
 
 function App() {
   return (
@@ -10,14 +11,22 @@ function App() {
       <BrowserRouter>
         <Nav />
         <Routes>
-          <Route path="/" element={<h1>Product Listing Component</h1>}></Route>
-          <Route path="/add" element={<h1>Add Products Component</h1>}></Route>
-          <Route
-            path="/update"
-            element={<h1>Update Products Component</h1>}
-          ></Route>
-          <Route path="/logout" element={<h1>Logout Component</h1>}></Route>
-          <Route path="/profile" element={<h1>Profile Component</h1>}></Route>
+          <Route element={<PrivateComponent />}>
+            <Route
+              path="/"
+              element={<h1>Product Listing Component</h1>}
+            ></Route>
+            <Route
+              path="/add"
+              element={<h1>Add Products Component</h1>}
+            ></Route>
+            <Route
+              path="/update"
+              element={<h1>Update Products Component</h1>}
+            ></Route>
+            <Route path="/logout" element={<h1>Logout Component</h1>}></Route>
+            <Route path="/profile" element={<h1>Profile Component</h1>}></Route>
+          </Route>
           <Route path="/signup" element={<SignUp />}></Route>
         </Routes>
         <Footer></Footer>
--- e-dashboard/frontend/src/components/SignUp.js
+++ e-dashboard/frontend/src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
@@ -6,6 +6,12 @@ export default function SignUp() {
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const navigate = useNavigate();
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  });
 
   const collectData = async () => {
     console.warn(name, email, password);
@@ -18,6 +24,7 @@ export default function SignUp() {
     });
     result = await result.json();
     console.warn(result);
+    localStorage.setItem("user", JSON.stringify(result));
     if (result) {
       navigate("/");
     }
