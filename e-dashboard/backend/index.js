6bd78e55e236f04f2bad8a7af47a33d90302c3ee
akhil <akhilamurarka@gmail.com>
2025-06-09 15:39:21 +0530

    login api

--- e-dashboard/backend/index.js
+++ e-dashboard/backend/index.js
@@ -8,9 +8,24 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/register", async (req, resp) => {
-  const user = new User(req.body);
-  const result = await user.save();
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result);
+});
+
+app.post("/login", async (req, resp) => {
+  if (req.body.password && req.body.email) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+      resp.send(user);
+    } else {
+      resp.send({ result: "No user found" });
+    }
+  } else {
+    resp.send({ result: "No user found" });
+  }
 });
 
 app.listen(5000);
