92d55c37c6dd91df2b4270c07946d4a935cf6c28
akhil <akhilamurarka@gmail.com>
2025-06-09 16:44:30 +0530

    navbar update

--- e-dashboard/frontend/src/components/Nav.js
+++ e-dashboard/frontend/src/components/Nav.js
@@ -10,46 +10,37 @@ function Nav() {
   };
   return (
     <div>
-      <ul className="nav-ul">
-        <li>
-          <Link to="/">Products</Link>
-        </li>
-        <li>
-          <Link to="/add">Add Product</Link>
-        </li>
-        <li>
-          <Link to="/update">Update Product</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        {/* <li>
-          {auth ? (
+      <img alt="logo" className="logo" src="https://w7.pngwing.com/pngs/621/196/png-transparent-e-commerce-logo-logo-e-commerce-electronic-business-ecommerce-angle-text-service.png"></img>
+      {auth ? (
+        <ul className="nav-ul">
+          <li>
+            <Link to="/">Products</Link>
+          </li>
+          <li>
+            <Link to="/add">Add Product</Link>
+          </li>
+          <li>
+            <Link to="/update">Update Product</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
             <Link to="/signup" onClick={logout}>
-              Logout
+              Logout ({JSON.parse(auth).name})
             </Link>
-          ) : (
+          </li>
+        </ul>
+      ) : (
+        <ul className="nav-ul nav-right">
+          <li>
             <Link to="/signup">SignUp</Link>
-          )}
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li> */}
-        {auth ? (
-          <Link to="/signup" onClick={logout}>
-            Logout
-          </Link>
-        ) : (
-          <>
-            <li>
-              <Link to="/signup">SignUp</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </>
-        )}
-      </ul>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
