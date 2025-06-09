d4faeff79933b022e16db7caf5f7a3092ea936ba
akhil <akhilamurarka@gmail.com>
2025-06-09 17:52:32 +0530

    integrate product list api

--- e-dashboard/frontend/src/App.js
+++ e-dashboard/frontend/src/App.js
@@ -6,6 +6,7 @@ import SignUp from "./components/SignUp";
 import PrivateComponent from "./components/PrivateComponent";
 import Login from "./components/Login";
 import AddProduct from "./components/AddProduct";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
@@ -14,10 +15,7 @@ function App() {
         <Nav />
         <Routes>
           <Route element={<PrivateComponent />}>
-            <Route
-              path="/"
-              element={<h1>Product Listing Component</h1>}
-            ></Route>
+            <Route path="/" element={<ProductList />}></Route>
             <Route path="/add" element={<AddProduct />}></Route>
             <Route
               path="/update"
