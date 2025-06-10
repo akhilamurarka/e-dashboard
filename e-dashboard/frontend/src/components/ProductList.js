e4a27f182fd7534ef8e3e1cacc7bba6f25fd8fc9
akhil <akhilamurarka@gmail.com>
2025-06-10 16:57:45 +0530

    search api integration

--- e-dashboard/frontend/src/components/ProductList.js
+++ e-dashboard/frontend/src/components/ProductList.js
@@ -23,9 +23,28 @@ function ProductList() {
     }
   };
 
+  const searchHandle = async (event) => {
+    let key = event.target.value;
+    if (key) {
+      let result = await fetch(`http://localhost:5000/search/${key}`);
+      result = await result.json();
+      if (result) {
+        setProducts(result);
+      }
+    } else {
+      getProducts();
+    }
+  };
+
   return (
     <div className="product-list">
       <h3>Product List</h3>
+      <input
+        type="text"
+        placeholder="search product"
+        className="input-product"
+        onChange={searchHandle}
+      ></input>
       <ul>
         <li>S.no</li>
         <li>Name</li>
@@ -33,7 +52,7 @@ function ProductList() {
         <li>Category</li>
         <li>Operation</li>
       </ul>
-      {products.map((item, index) => (
+      {products.length>0?products.map((item, index) => (
         <ul key={item._id}>
           <li>{index + 1}</li>
           <li>{item.name}</li>
@@ -44,7 +63,7 @@ function ProductList() {
             <Link to={"/update/" + item._id}>Update</Link>
           </li>
         </ul>
-      ))}
+      )):<h1>No products found</h1>}
     </div>
   );
 }
