3d84f432f7a3c8ffe8c1676d7831774391e62bbd
akhil <akhilamurarka@gmail.com>
2025-06-10 15:50:44 +0530

    api for get single product

--- e-dashboard/backend/index.js
+++ e-dashboard/backend/index.js
@@ -49,4 +49,13 @@ app.delete("/product/:id", async (req, resp) => {
   resp.send(result);
 });
 
+app.get("/product/:id", async (req, resp) => {
+  const result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    resp.send(result);
+  } else {
+    resp.send({ result: "No record found." });
+  }
+});
+
 app.listen(5000);
