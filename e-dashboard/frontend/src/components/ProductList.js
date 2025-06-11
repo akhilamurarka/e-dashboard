04685d57daaf6b67b65a57788b6a29745683d186
akhil <akhilamurarka@gmail.com>
2025-06-11 15:04:23 +0530

    handle auth token in reactjs

--- e-dashboard/frontend/src/components/ProductList.js
+++ e-dashboard/frontend/src/components/ProductList.js
@@ -8,7 +8,11 @@ function ProductList() {
   }, []);
 
   const getProducts = async () => {
-    let result = await fetch("http://localhost:5000/products");
+    let result = await fetch("http://localhost:5000/products", {
+      headers: {
+        Authorization: JSON.parse(localStorage.getItem("token")),
+      },
+    });
     result = await result.json();
     setProducts(result);
   };
@@ -52,18 +56,22 @@ function ProductList() {
         <li>Category</li>
         <li>Operation</li>
       </ul>
-      {products.length>0?products.map((item, index) => (
-        <ul key={item._id}>
-          <li>{index + 1}</li>
-          <li>{item.name}</li>
-          <li>$ {item.price}</li>
-          <li>{item.category}</li>
-          <li>
-            <button onClick={() => handleDelete(item._id)}>Delete</button>
-            <Link to={"/update/" + item._id}>Update</Link>
-          </li>
-        </ul>
-      )):<h1>No products found</h1>}
+      {products.length > 0 ? (
+        products.map((item, index) => (
+          <ul key={item._id}>
+            <li>{index + 1}</li>
+            <li>{item.name}</li>
+            <li>$ {item.price}</li>
+            <li>{item.category}</li>
+            <li>
+              <button onClick={() => handleDelete(item._id)}>Delete</button>
+              <Link to={"/update/" + item._id}>Update</Link>
+            </li>
+          </ul>
+        ))
+      ) : (
+        <h1>No products found</h1>
+      )}
     </div>
   );
 }
